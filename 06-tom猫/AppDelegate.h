a8ad5a61b96b1b9eafdadf63fc87d386719338e7
xiaoli <xiaoli@douniwan.com>
2015-11-28 15:51:28 +0800

    Initial Commit

---
+++ 06-tom猫/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  06-tom猫
+//
+//  Created by apple on 15/11/28.
+//  Copyright © 2015年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 06-tom猫/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  06-tom猫
+//
+//  Created by apple on 15/11/28.
+//  Copyright © 2015年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
